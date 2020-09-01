9b635e557ec01aa876729e4fb7fa3cc08f588185
Zhongyang MA <mzy@mail.ustc.edu.cn>
2020-09-01 09:46:47 +0800

    add api

---
+++ src/main/java/com/solarwind/controller/ApiController.java
@@ -0,0 +1,39 @@
+package com.solarwind.controller;
+
+import com.solarwind.model.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Zhongyang MA
+ * @date Sep/01/2020
+ */
+@RestController
+@RequestMapping(value = "/api")
+public class ApiController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/users")
+    public List<User> getUsers() {
+
+        User user1 = new User();
+        user1.setName("Stephen Hawking");
+        user1.setGender("Male");
+        user1.setOccupation("Scientist");
+
+        User user2 = new User();
+        user2.setName("Donald Trump");
+        user2.setGender("Male");
+        user2.setOccupation("President of the US");
+
+        List list = new ArrayList();
+        list.add(user1);
+        list.add(user2);
+
+        return list;
+    }
+
+}
---
+++ src/main/java/com/solarwind/model/User.java
@@ -0,0 +1,18 @@
+package com.solarwind.model;
+
+import lombok.Data;
+
+/**
+ * @author Zhongyang MA
+ * @date Sep/01/2020
+ */
+@Data
+public class User {
+
+    private String name;
+
+    private String gender;
+
+    private String occupation;
+
+}
