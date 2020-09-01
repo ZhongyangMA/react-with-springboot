b330a71403814ca650a5b423f9659a42f6160322
Zhongyang MA <mzy@mail.ustc.edu.cn>
2020-09-01 15:11:58 +0800

    react build static

---
+++ src/main/resources/static/precache-manifest.2fbdec7e91b35368aecfb2b4de6139b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c33180598bfcf1c3eb7455b92cf69143",
+    "url": "./index.html"
+  },
+  {
+    "revision": "26e1016636cfb835927f",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e9209b13124d90e74513",
+    "url": "./static/js/2.0abee985.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0abee985.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26e1016636cfb835927f",
+    "url": "./static/js/main.80a98ef0.chunk.js"
+  },
+  {
+    "revision": "30e28055fd24d693db5d",
+    "url": "./static/js/runtime-main.78fa3a72.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
